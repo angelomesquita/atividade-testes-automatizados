95b9abdca1c6dd54492c044356906209baea471c
vanderson1022 <vanderson1022@gmail.com>
2023-12-04 20:01:40 -0300

    trabalho da aula

---
+++ Francisco/praticajavaprojeto/src/main/java/br/com/acelerajava/App.java
@@ -0,0 +1,13 @@
+package br.com.acelerajava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
